a7b2924a855a5b6b186e6cedc7a640bf0d226d82
dayatpribadi <51143967+dayatpribadi@users.noreply.github.com>
2024-10-23 14:06:25 +0700

    update-page-apexchart TPP
    update-page-apexchart TPP

--- assets/js/dashboard-opd/semesta.js
+++ assets/js/dashboard-opd/semesta.js
@@ -135,3 +135,126 @@ var options = {
 
   var chart = new ApexCharts(document.querySelector("#chartLkhPerOpd"), options);
   chart.render();
+
+
+//    CHartLine2 //
+var options = {
+    series: [
+    {
+      name: "Realisasi - 2024",
+      data: [28, 29, 33, 36, 32, 32, 33]
+    },
+    {
+      name: "Penghematan - 2024",
+      data: [12, 11, 14, 18, 17, 13, 13]
+    }
+  ],
+    chart: {
+    height: 350,
+    type: 'line',
+    dropShadow: {
+      enabled: true,
+      color: '#000',
+      top: 18,
+      left: 7,
+      blur: 10,
+      opacity: 0.2
+    },
+    zoom: {
+      enabled: false
+    },
+    toolbar: {
+      show: false
+    }
+  },
+  colors: ['#0F98E7', '#00A91B'],
+  dataLabels: {
+    enabled: true,
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  title: {
+    text: 'Realisasi Pagu TPP',
+    align: 'left'
+  },
+  grid: {
+    borderColor: '#e7e7e7',
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      opacity: 0.5
+    },
+  },
+  markers: {
+    size: 1
+  },
+  xaxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    title: {
+      text: 'Month'
+    }
+  },
+  yaxis: {
+    title: {
+      text: 'TPP'
+    },
+    min: 5,
+    max: 40
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'right',
+    floating: true,
+    offsetY: -25,
+    offsetX: -5
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chartLine2"), options);
+  chart.render();
+
+
+
+  var options = {
+    series: [
+        {
+            name: "Realisasi",
+            data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        },
+        {
+            name: "Penghematan",
+            data: [102, 95, 19, 159, 69, 82, 99, 191, 228]
+        },
+    ],
+    chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  title: {
+    text: 'Realisasi Pagu TPP',
+    align: 'left'
+  },
+  grid: {
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      opacity: 0.5
+    },
+  },
+  xaxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#realisasiPaguTpp"), options);
+  chart.render();
+
+
