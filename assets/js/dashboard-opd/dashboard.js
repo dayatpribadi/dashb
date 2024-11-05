a91167584fcfba2800d78a8c8f8536e9696fa8bb
dayatpribadi <51143967+dayatpribadi@users.noreply.github.com>
2024-11-05 13:49:11 +0700

    Update dashboard.js

--- assets/js/dashboard-opd/dashboard.js
+++ assets/js/dashboard-opd/dashboard.js
@@ -112,11 +112,6 @@ $(document).ready(function () {
   });
 
 
-  Nama : FERDY MEIROFA EKA SAPUTRA
-  NIP : 198305242006041005
-  Jabatan : Kepala Bidang Aplikasi Informatika
-
-
   // CHART PRESENSI
 var options = {
   series: [44, 55],
